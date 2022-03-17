48e87721e6928ea302fa7953722eaa81d1c743bf
rafaelimao <monteiro.rafael2008@gmail.com>
2022-03-17 20:05:35 -0300

    Create a basic Next project

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import home from "../components/home.module.css";
+
+export default () => {
+    return (
+        <div className={home.main}>
+            <h1>Hello World!</h1>
+        </div>
+    )
+}
